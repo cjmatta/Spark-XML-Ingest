135a6791a5dc6322552970de1197f9b809731cc9
Chris Matta <cmatta@mapr.com>
2015-12-04 11:03:19 -0500

    Added --files for config file

--- src/main/resources/submit_job.sh
+++ src/main/resources/submit_job.sh
@@ -1,7 +1,7 @@
 JARFILE=/Users/cmatta/Downloads/Spark-XML-Ingest/target/scala-2.11/Spark-XML-Ingest-assembly-0.1-SNAPSHOT.jar
 CLASSNAME=com.mapr.xml2json.XML2Json
 MASTER="local[2]"
-CONFIGFILE=$0
+CONFIGFILE=$1
 
 if [[ ! -f $CONFIGFILE ]]; then
     echo "Config file not found!"
@@ -13,4 +13,4 @@ if [[ -z $SPARK_HOME ]]; then
     exit 1
 fi
 
-$SPARK_HOME/bin/spark-submit --master=$MASTER --class $CLASSNAME $JARFILE $CONFIGFILE
+$SPARK_HOME/bin/spark-submit --master=$MASTER --class $CLASSNAME --files $CONFIGFILE $JARFILE
