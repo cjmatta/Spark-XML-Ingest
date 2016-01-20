29b30ea157277fee9ddc06359d748877b0bd98f0
Chris Matta <cmatta@mapr.com>
2016-01-20 16:49:12 -0500

    fixed how to specify application.conf in submit_job.sh

--- submit_job.sh
+++ submit_job.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 JARFILE=/Users/cmatta/Downloads/Spark-XML-Ingest/target/scala-2.11/Spark-XML-Ingest-assembly-0.1-SNAPSHOT.jar
 CLASSNAME=com.mapr.xml2json.XML2Json
 MASTER="local[2]"
@@ -13,4 +14,4 @@ if [[ -z $SPARK_HOME ]]; then
     exit 1
 fi
 
-$SPARK_HOME/bin/spark-submit --master=$MASTER --class $CLASSNAME --files $CONFIGFILE $JARFILE
+$SPARK_HOME/bin/spark-submit  --driver-java-options "-Dconfig.file=${CONFIGFILE}" --master=$MASTER --class $CLASSNAME  $JARFILE
